e3e899c05d3e1fbd0567a5aaeb85e60c431c20cf
Nicholas Tidwell <nicktidwell52@gmail.com>
2020-02-13 09:48:25 -0500

    add gui, and table for data

--- MainForm.py
+++ MainForm.py
@@ -1,18 +1,18 @@
 from tkinter import *
 from buttonEvents import clicked, loadStockClicked
 from functools import partial
+from pandastable import Table
 
 
 window = Tk()
-window.geometry('1000x1000')
+window.geometry('400x100')
 window.title("Python Stock Market")
-
 
 
 lbl = Label(window, text="Search New Stock")
 txt = Entry(window, width = 10)
 
-clicked_w_args = partial(loadStockClicked, txt, window)
+clicked_w_args = partial(loadStockClicked, txt)
 btn = Button(window, text="Search", command=clicked_w_args)
 
 lbl.grid(column=0, row=0)
---
+++ csvTable.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from pandastable import Table
+
+class TestApp(Frame):
+
+        def __init__(self, csvName, master):
+            Frame.__init__(self, master)
+            self.main = master
+            self.main.geometry('800x600+200+100')
+            self.main.title(csvName + " ticker data")
+            f = Frame(self.main)
+            f.pack(fill=BOTH,expand=1)
+            self.table = pt = Table(f)
+            pt.importCSV('StockData/' + csvName + '.csv')
+            pt.show()
+            return
+
+
