65237c1b2ff01508bc459b87c79f4364da3a4891
okosa <gparolI27>
2020-04-13 15:21:43 -0400

    Added Prediction Plotter and optimized model

--- StockPrediction/LSTM/CreateTrainingData.py
+++ StockPrediction/LSTM/CreateTrainingData.py
@@ -3,35 +3,39 @@
 
 import numpy as np
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-index_of_target_variable = 3  # close price
+index_of_target_variable = 4  # close price
 
 
 def create_timeseries(data, time_steps_in_batch):
     num_batches = data.shape[0] - time_steps_in_batch
-    entries_per_sample = data.shape[1]
+    entries_per_sample = data.shape[1]-1
     x = np.zeros((num_batches, time_steps_in_batch, entries_per_sample))
     y = np.zeros((num_batches,))
+    dates = np.zeros((num_batches,), dtype=np.dtype('a16'))
     for i in range(num_batches):
-        x[i] = data[i:i + time_steps_in_batch]
-        y[i] = data[i + time_steps_in_batch, index_of_target_variable]
-    return x, y
+        x[i] = data.iloc[i:i + time_steps_in_batch, 1:]
+        y[i] = data.iloc[i + time_steps_in_batch, index_of_target_variable]
+        dates[i] = data.iloc[i + time_steps_in_batch, 0]
+    return x, y, dates
 
 
 def generate_training_data(stock_name, time_steps_in_batch):
     data_source = f'../../StockData/{stock_name}.csv'
 
-    data = np.genfromtxt(data_source, delimiter=',')
-    data = np.delete(data, 0, axis=0)  # delete labels
-    data = np.delete(data, 0, axis=1)  # delete dates
-
+    data = pd.read_csv(data_source)
     # normalize each column
-    data = data / data.max(axis=0)
+    data2 = data.iloc[:, 1:]
+    data.iloc[:, 1:] = (data2-data2.min())/(data2.max()-data2.min())
 
     train_data, test_data = train_test_split(data, train_size=.8, test_size=.2, shuffle=False)
 
-    x_train, y_train = create_timeseries(train_data, time_steps_in_batch)
-    x_test, y_test = create_timeseries(test_data, time_steps_in_batch)
+    x_train, y_train, __ = create_timeseries(train_data, time_steps_in_batch)
+    x_test, y_test, dates = create_timeseries(test_data, time_steps_in_batch)
+    np.save('TestingX', x_test)
+    np.save('TestingY', y_test)
+    np.save('TestingDates', dates)
     return x_train, y_train, x_test, y_test
 
 
@@ -39,5 +43,3 @@ if __name__ == '__main__':
     x_train, y_train, x_test, y_test = generate_training_data('AAL', 10)
     np.save('TrainingX', x_train)
     np.save('TrainingY', y_train)
-    np.save('TestingX', x_test)
-    np.save('TestingY', y_test)
