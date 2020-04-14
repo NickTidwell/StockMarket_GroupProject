7ca1eb3d91c62686b22a4ccb3bf638c390838c9a
Nicholas Tidwell <nicktidwell52@gmail.com>
2020-04-14 13:28:39 -0400

    added plotting graph to GUI

--- MainForm.py
+++ MainForm.py
@@ -10,27 +10,41 @@ window.geometry('600x400')
 window.title("Python Stock Market")
 
 
-lbl_searchText = Label(window, text="Enter Stock To Search: ")
+lbl_searchText = Label(window, text="Enter Stock To Display: ")
 startData_lbl = Label(window, text="Start Day")
 endDate_lbl = Label(window, text="End Date")
+lbl_importName = Label(window, text="Enter stock to import")
+entry_import = Entry(window, width = 15)
 
 entry_stock = Entry(window, width = 15)
 
 startCal = DateEntry(window)
 endCal = DateEntry(window)
 
-click_searchStock = partial(be.loadStockClicked, entry_stock, startCal, endCal)
-btn_searchStock = Button(window, text="Search", width=10, command=click_searchStock)
+click_searchStock = partial(be.loadStockClicked, entry_stock)
+btn_searchStock = Button(window, text="Display Data", width=10, command=click_searchStock)
+
+click_graph = partial(be.plotStock, entry_stock)
+btn_graph = Button(window, text="Graph Data", width=15, command=click_graph)
+
+click_import = partial(be.importSingleStock, entry_import, startCal, endCal)
+btn_import = Button(window, text="Import", width=10, command=click_import)
 
 btn_import500 = Button(window, text="Import Fortune 500", width = 15, command=be.importStockClicked)
 
-startData_lbl.grid(column=0, row=1)
-endDate_lbl.grid(column=0, row=2)
-endCal.grid(column=1, row=2)
-startCal.grid(column=1, row=1)
+
 lbl_searchText.grid(column=0, row=0)
 btn_searchStock.grid(column=2, row=0)
 entry_stock.grid(column=1, row=0)
-btn_import500.grid(column=5, row=0)
+btn_import500.grid(column=3, row=2)
+btn_graph.grid(column=3,row=0)
 
+
+lbl_importName.grid(column=0,row=2)
+entry_import.grid(column=1,row=2)
+startData_lbl.grid(column=0, row=3)
+endDate_lbl.grid(column=0, row=4)
+btn_import.grid(column=2, row=2)
+endCal.grid(column=2, row=4)
+startCal.grid(column=2, row=3)
 window.mainloop()
--- buttonEvents.py
+++ buttonEvents.py
@@ -3,18 +3,40 @@ from tkinter import *
 import importForm
 import csv
 import csvTable
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import pandas as pd
 
 def displayGrid(txt):
     dow = Tk()
     app = csvTable.CreateTable(txt, dow)
     app.mainloop()
 
-def loadStockClicked(txt, start, end):
-    loadStock(txt.get(), start.get_date(), end.get_date())
+def loadStockClicked(txt):
     displayGrid(txt.get())
 
 def importStockClicked():
     importForm.ImportFrame()
-    
+
+def importSingleStock(txt,start,end):
+    loadStock(txt.get(),start.get_date(),end.get_date())
+
+def plotStock(txt):
+    #Modfied from code in Playground PlotStock.py
+    stock_name = txt.get()
+    y_value = 'Open'
+    data_file_path = f'StockData/{stock_name}.csv'
+    data = pd.read_csv(data_file_path)
+    title = f'{stock_name.upper()} Stock'
+    plt.figure(figsize=(12, 6), num=title)
+    ax = plt.subplot(1, 1, 1)
+    plt.title(title)
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(10))
+    ax.xaxis.set_minor_locator(ticker.MaxNLocator(100))
+    plt.xlabel('Date')
+    plt.ylabel(f'{y_value} Price ($)')
+    ax.plot(data['Date'], data[y_value])
+    plt.show()
+
     
 
