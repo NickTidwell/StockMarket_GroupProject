e79dc40eb153b9b707844e8985073c67f1ad4392
okosa <gparolI27>
2020-04-20 19:25:25 -0400

    Code cleanup

--- StockPrediction/LSTM/PredictStock.py
+++ StockPrediction/LSTM/PredictStock.py
@@ -5,14 +5,8 @@ import pandas as pd
 from keras.models import load_model
 import numpy as np
 from StockPrediction.LSTM.CreateTrainingData import process_data, denormalize
-import datetime
 from os import path
 
-
-def next_day(date):
-    d = datetime.datetime.strptime(date, '%Y-%m-%d')
-    d -= datetime.timedelta(days=1)
-    return d.strftime('%Y-%m-%d')
 
 
 def predict_stocks(stock_data):
@@ -30,12 +24,6 @@ def predict_stocks(stock_data):
                                                      model_metadata['time_steps_in_batch'])
     predictions = model.predict(x, batch_size=model_metadata['batch_size']).flatten()
     predictions = denormalize(predictions, min_price, max_price)
-
-    # increment dates by one
-    # might use later for predictions multiple days in the future
-    # ates = np.append(next_day(dates[0].astype('U')).encode(), dates)
-    # dates = dates[(dates.size-predictions.size):]
-    # dates = dates[:-1]
 
     results = pd.DataFrame({'Date': dates, 'Prediction': predictions})
     results['Date'] = results['Date'].str.decode('utf-8')
